6c50d21a4e63def8a78936e9c719d1fa20dcfb8f
jiroyamada <poirot343@outlook.jp>
2021-08-12 22:50:09 +0900

    フォーム作成

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-prompt(111+222)
+document.getElementById("greeting-button").onclick = function(){
+
+    var yourname = document.getElementById("name").value;
+
+    document.getElementById("box").innerHTML = "こんにちは、" + yourname + "さん";
+};
