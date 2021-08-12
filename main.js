b39ba96d5671618cf00c3c85efffcc26ca26178f
jiroyamada <poirot343@outlook.jp>
2021-08-12 23:15:49 +0900

    複数フォーム

--- main.js
+++ main.js
@@ -18,3 +18,24 @@ document.getElementById("greeting-button").onclick = function(){
 
     document.getElementById("box").innerHTML = greeting + "、" + targetname + "さん";
 };
+
+document.getElementById("cheerful-button").onclick = function(){
+
+    var targetname = document.getElementById("name").value;
+    var hour = new Date().getHours();
+    var greeting;
+
+    console.log("現在の時間："+ hour + "時");
+
+    if(6 <= hour && hour < 12){
+        greeting = "おはよう";
+    }
+    else if(12 <= hour && hour < 18){
+        greeting = "こんにちは";
+    }
+    else{
+        greeting = "こんばんは";
+    }
+
+    document.getElementById("box").innerHTML = greeting + "！、" + targetname + "さん！！！";
+};
