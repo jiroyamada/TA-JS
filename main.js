aed0152f946a4d26148e917f2ff54248b498d9d1
jiroyamada <poirot343@outlook.jp>
2021-08-13 09:47:30 +0900

    関数のフォーム

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-document.getElementById("greeting-button").onclick = function(){
-
-    var targetname = document.getElementById("name").value;
-    var hour = new Date().getHours();
+function greet(){
     var greeting;
-
-    console.log("現在の時間："+ hour + "時");
+    var hour = new Date().getHours();
 
     if(6 <= hour && hour < 12){
         greeting = "おはよう";
@@ -15,27 +11,21 @@ document.getElementById("greeting-button").onclick = function(){
     else{
         greeting = "こんばんは";
     }
+
+    return greeting;
+
+}
+
+document.getElementById("greeting-button").onclick = function(){
+    var greeting = greet();
+    var targetname = document.getElementById("name").value;
 
     document.getElementById("box").innerHTML = greeting + "、" + targetname + "さん";
 };
 
 document.getElementById("cheerful-button").onclick = function(){
-
+    var greeting = greet();
     var targetname = document.getElementById("name").value;
-    var hour = new Date().getHours();
-    var greeting;
-
-    console.log("現在の時間："+ hour + "時");
-
-    if(6 <= hour && hour < 12){
-        greeting = "おはよう";
-    }
-    else if(12 <= hour && hour < 18){
-        greeting = "こんにちは";
-    }
-    else{
-        greeting = "こんばんは";
-    }
 
     document.getElementById("box").innerHTML = greeting + "！、" + targetname + "さん！！！";
 };
